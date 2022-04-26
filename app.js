2ffbeb6ac96ec10255f07e47c03a6850eb6609d1
LucasFasolato <89876311+LucasFasolato@users.noreply.github.com>
2022-04-26 18:15:43 -0300

    Add navbar with Bootstrap

--- app.js
+++ app.js
@@ -1,40 +1,56 @@
+// const getImport = document.getElementById('#navbar-file'); 
+// // document.getElementById('contenedor').appendChild(getImport); 
+// document.getElementById('contenedor').innerHTML = getImport;
 let pagina = 1;
 const btnAnterior = document.getElementById('btnAnterior');
 const btnSiguiente = document.getElementById('btnSiguiente');
 let total_pages;
+let num=0;
+let id = [];
 btnSiguiente.addEventListener('click', () => {
     if (pagina < total_pages){
         pagina += 1;
         cargarPeliculas();
     }
 });
-
 btnAnterior.addEventListener('click', () => {
     if (pagina > 1){
         pagina -= 1;
         cargarPeliculas();
     }
 });
+
+
 const cargarPeliculas = async () => {
     try {
         const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=81733fbe56cb4b598fe53cdb888c5fe8&language=es-AR&page=${pagina}`);
         console.log(respuesta);
+        
         if(respuesta.status === 200) {
             let peliculas = '';
             const datos = await respuesta.json();
+            console.log(datos);
             total_pages = datos.total_pages;
             datos.results.forEach(pelicula => {
                 peliculas += `
-                <div class="pelicula">
-                    <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
-                    <h3 class="titulo">${pelicula.title}</h3>
+                <div class="pelicula" id=${pelicula.id}>
+                    <a href="./pelicula-info.html?id=${pelicula.id}">
+                        <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+                        <h3 class="titulo">${pelicula.title}</h3>
+                    </a>
                 </div>
                 `;
+                id.push(`${pelicula.id}`);
             });
-
+            console.log(id);
+            // pelicula.addEventListener('click', function () {
+            //     window.location.href=`./pelicula-info.html`
+            // })
             document.getElementById('contenedor').innerHTML = peliculas;
+
         } else if(respuesta.status === 401) {
             console.log("Error en la conexion. Bad keyAddress");
+
         } else if(respuesta.status === 404) {
             console.log("No se encontraron las peliculas solicitadas.");
         }
---
+++ pelicula-info.js
@@ -0,0 +1,7 @@
+const pelicula = document.getElementById("1");
+
+// pelicula.addEventListener('click', ()=>{
+//     alert("Se ha hecho click");
+// });
+
+console.log(pelicula);
